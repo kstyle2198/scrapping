d1ce0efde3c3804b9f4653cc14524105397e7d10
Kstyle0710 <63958865+Kstyle0710@users.noreply.github.com>
2021-09-23 16:49:53 +0900

    gensim test

--- summarize.py
+++ summarize.py
@@ -6,15 +6,22 @@ import requests
 from gensim.summarization.summarizer import summarize
 from gensim.summarization import keywords
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"} 
+headers = {
+    "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36"
+}
+
 
 def get_only_text(url):
     res = requests.get(url, headers=headers)
     res.raise_for_status
     soup = BeautifulSoup(res.text, "lxml")
     title = soup.title.get_text()
-    content = soup.find("div", {"id": "articleBodyContents"}).get_text().strip()
+    content = soup.find("div", {
+        "id": "articleBodyContents"
+    }).get_text().strip()
     return title, content
+
 
 ## navar 뉴스 링크
 # url = "https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=101&oid=014&aid=0004712835"
@@ -22,12 +29,9 @@ url = "https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=104&oid=028&
 target = get_only_text(url)
 print(len(target[1]))
 print(target[1])
-print("="*20)
+print("=" * 20)
 print("title : " + target[0])
 print("Summary : ")
 summary = summarize(repr(target[1]), word_count=90)
 print(summary)
 print(len(summary))
-
-
-
